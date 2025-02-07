6c5f2c07518ba8ac69dcd436e85c0f965c83d0c9
Tom Smith <tom@craftscale.uk>
2025-02-07 09:08:53 +0000

    Added correct API calls

--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+import requests
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=480dc4bb43512f39def7b2db34d39522'
+    city = 'Leeds'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+
+    # city_weather.temp = (city_weather.temp - 32) * 5/9
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon'],
+    }
+
+
+    temperature = city_weather['main']['temp']
+    temperature = (temperature - 32) * 5/9
+    print(temperature)
+
+    context = {'weather' : weather}
+
+    print(weather)
+
+
+
     return render(request, 'home.html')
--- myProject/settings.py
+++ myProject/settings.py
@@ -100,6 +100,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
