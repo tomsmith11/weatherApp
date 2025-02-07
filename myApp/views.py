2520ce16969c2069d788cd4d752bec57b2efb8b6
Tom Smith <tom@craftscale.uk>
2025-02-07 09:14:03 +0000

    Updated views and home

--- myApp/views.py
+++ myApp/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
 import requests
 import logging
+from .models import City
+
 logger = logging.getLogger(__name__)
 
 # Create your views here.
 def home(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=480dc4bb43512f39def7b2db34d39522'
     city = 'Leeds'
-    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
 
 
-    # city_weather.temp = (city_weather.temp - 32) * 5/9
+    cities = City.object.all()
+    weather_data = []
+    
+    for city in cities:
 
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon'],
-    }
+      city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+      weather = {
+          'city' : city,
+          'temperature' : city_weather['main']['temp'],
+          'description' : city_weather['weather'][0]['description'],
+          'icon' : city_weather['weather'][0]['icon'],
+      }
+
+      weather_data.append(weather)
 
 
     temperature = city_weather['main']['temp']
     temperature = (temperature - 32) * 5/9
     print(temperature)
 
-    context = {'weather' : weather}
+    context = {'weather_data' : weather_data}
 
     print(weather)
-
-
-
     return render(request, 'home.html')
